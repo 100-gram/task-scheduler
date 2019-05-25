97dbe986e7cd6d56566265658977c52706cea0a1
Arkadiy <arkadiy@ex.ua>
2019-05-25 18:31:57 +0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 
@@ -8,5 +8,10 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
