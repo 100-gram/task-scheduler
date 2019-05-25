0d24133bc9277c55ad1a7b1db6f34a07e1d138c6
Oleksiy Atamaniuk <alexata1204@gmail.com>
2019-05-25 23:54:45 +0300

    JSON serialization

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, make_response, jsonify, request
+from model.task import Task
+from os import path
+import json
+import simplejson
 
 app = Flask(__name__)
 
+storage_path = path.dirname(path.realpath(__file__)) + '\\data\\tasks.json'
 
 @app.route('/')
 def hello_world():
@@ -30,4 +35,20 @@ def add_income():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    task = Task('name1', 'description1', "Aug 28 1999 12:00AM", "Aug 30 1999 12:00AM", False)
+    task3 = Task('name2', 'description2', "Aug 28 1999 12:00AM", "Aug 30 1999 12:00AM", False)
+    task2 = Task('name3', 'description3', "Aug 28 1999 12:00AM", "Aug 30 1999 12:00AM", False)
+    task4 = Task.create('name4', 'ebgkerngk', "Aug 28 1999 12:00AM", 35)
+    task5 = Task.create('name5', 'ebgkerngk', "Aug 28 1999 12:00AM", 35)
+    task6 = Task.create('name6', 'ebgkerngk', "Aug 28 1999 12:00AM", 35)
+    dic = [task, task2, task3, task4, task5, task6]
+    with open(storage_path, 'w') as outfile:
+        simplejson.dump(dic, outfile, indent=4, for_json=True)
+    new_str = open(storage_path, 'r').read()
+    print(new_str)
+    obj2_dict = simplejson.loads(new_str)
+    dict_new = []
+    for val in obj2_dict:
+        dict_new.append(Task.from_json(val))
+    for val in dict_new:
+        print(val.name + val.description)
--- model/task.py
+++ model/task.py
@@ -1,10 +1,34 @@
 from dateutil import parser
+from datetime import timedelta
 
 
 class Task:
-    def __init__(self, name, description, date_start, duration):
+    def __init__(self, id, name, description, date_start, date_end, is_complited):
         self.name = name
         self.description = description
-        self.date_start = parser.parse(date_start)
-        self.date_end = self.date_start + duration
-        self.is_complited = False
+        self.date_start = date_start
+        self.date_end = date_end
+        self.is_complited = is_complited
+
+    def __json__(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'date_start': str(self.date_start),
+            'date_end': str(self.date_end),
+            'is_complited': self.is_complited
+        }
+
+    for_json = __json__  # supported by simplejson
+
+    @classmethod
+    def from_json(cls, json_obj):
+        obj = cls(json_obj['name'], json_obj['description'], parser.parse(json_obj['date_start']),
+                  parser.parse(json_obj['date_end']),
+                  json_obj['is_complited'])
+        return obj
+
+    @classmethod
+    def create(cls, name, description, date_start, duration):
+        return cls(name, description, parser.parse(date_start), parser.parse(date_start) + timedelta(minutes=duration),
+                   False)
