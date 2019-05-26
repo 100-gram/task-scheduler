b2e138b19699cc0b71c44a2d9ffb50a15203adef
Arkadiy <arkadiy@ex.ua>
2019-05-26 23:40:23 +0300

    api fix return codes

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 from os import path, system, name
 from sys import modules
-from flask import request
+from flask import request, Response
+import simplejson
 
 app_path = path.dirname(modules['__main__'].__file__)
 
@@ -19,3 +20,16 @@ def shutdown_server():
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
+
+
+def json_response(data, status=200):
+    json = simplejson.dumps(data, indent=4, for_json=True)
+    return Response(json, status=status, mimetype='application/json')
+
+
+def query_pagination_params():
+    return {
+        'offset': request.args.get('offset'),
+        'limit': request.args.get('limit'),
+        'query': request.args.get('query')
+    }
