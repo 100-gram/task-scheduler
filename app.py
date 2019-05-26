9ebf9601741059a9b12f7f31fadfcf27bb06c37c
Arkadiy <arkadiy@ex.ua>
2019-05-27 02:55:26 +0300

    api final

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, make_response, jsonify
+from flask import make_response, jsonify
 from controller.datamanager import DataManager
 from werkzeug.exceptions import HTTPException
 from app.server import Server
 from router.api import api
+import logging
+
 
 app = Server(__name__)
-
+log = logging.getLogger('werkzeug')
+log.disabled = True
+app.logger.disabled = True
 
 app.register_blueprint(api, url_prefix='/api/v1')
 app.config['data_manager'] = DataManager.load_from_file()
@@ -17,8 +21,8 @@ def hello_world():
 
 
 @app.errorhandler(HTTPException)
-def bad_request(error):
-    return make_response(jsonify({'error': error}), error.code)
+def error_handler(error):
+    return make_response(jsonify({'error': error.description}), error.code)
 
 
 if __name__ == '__main__':
--- app/server.py
+++ app/server.py
@@ -4,6 +4,13 @@ from config.config import clear, shutdown_server
 import threading
 import time
 
+"""
+This app module is wrapper for base flask application.
+
+Here interface module is importing to initialize you want in background in separate thread.
+Just set your code after run_job
+"""
+
 
 class Server(Flask):
     def __init__(self, *args, **kwargs):
@@ -16,8 +23,7 @@ class Server(Flask):
             time.sleep(1)
             clear()
             ConsoleInterface.console_init()
+            shutdown_server()
 
         t = threading.Thread(target=run_job)
         t.start()
-        t.join()
-        shutdown_server()
