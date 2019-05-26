b9209472087ffaf9b4fc2b20b00d7a4e9004ea10
Arkadiy <arkadiy@ex.ua>
2019-05-26 17:23:20 +0300

    make controller and app context

--- router/api.py
+++ router/api.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, Response, jsonify, abort, request
-from model.datamanager import DataManager
+from flask import Blueprint, Response, jsonify, abort, request, current_app
+from controller.datamanager import DataManager
 import simplejson
 
-api = Blueprint('api/v1/', __name__)
+api = Blueprint('api', __name__)
 
 
 @api.route('/')
@@ -12,14 +12,15 @@ def index():
 
 @api.route('/tasks', methods=['GET'])
 def get_tasks():
-    DataManager.get_all()
-    data = simplejson.dumps({"tasks": DataManager.get_all()}, indent=4, for_json=True)
+    storage = current_app.config["data_manager"]
+    data = simplejson.dumps({"tasks": storage.get_all()}, indent=4, for_json=True)
     return Response(data, status=200, mimetype='application/json')
 
 
 @api.route('/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    item = list(filter(lambda t: t.task_id == task_id, DataManager.get_all()))
+    storage = current_app.config["data_manager"]
+    item = list(filter(lambda t: t.task_id == task_id, storage.get_all()))
     if len(item) == 0:
         abort(404)
     data = simplejson.dumps({"task": item}, indent=4, for_json=True)
@@ -28,8 +29,9 @@ def get_task(task_id):
 
 @api.route('/tasks/complete/<int:task_id>', methods=['GET'])
 def complete_task(task_id):
-    if DataManager.complete_task(task_id):
-        item = list(filter(lambda t: t.task_id == task_id, DataManager.get_all()))
+    storage = current_app.config["data_manager"]
+    if storage.complete_task(task_id):
+        item = list(filter(lambda t: t.task_id == task_id, storage.get_all()))
         data = simplejson.dumps({"task": item}, indent=4, for_json=True)
         return Response(data, status=200, mimetype='application/json')
     else:
@@ -38,6 +40,7 @@ def complete_task(task_id):
 
 @api.route('/tasks', methods=['POST'])
 def create_task():
+    storage = current_app.config["data_manager"]
     if not request.json or 'name' not in request.json or 'description' not in request.json \
             or 'date_start' not in request.json or'duration' not in request.json:
         abort(400)
@@ -46,7 +49,7 @@ def create_task():
     _date_start = request.json['date_start']
     _duration = request.json['duration']
     if _name and _description and _date_start and _duration:
-        task = DataManager.create_task(_name, _description, _date_start, _duration)
+        task = storage.create_task(_name, _description, _date_start, _duration)
         data = simplejson.dumps({"task": task}, indent=4, for_json=True)
         return Response(data, status=201, mimetype='application/json')
     else:
@@ -55,7 +58,8 @@ def create_task():
 
 @api.route('/tasks/<int:task_id>', methods=['DELETE'])
 def delete_item(task_id):
-    if DataManager.delete_task(task_id):
+    storage = current_app.config["data_manager"]
+    if storage.delete_task(task_id):
         return jsonify({'result': True})
     else:
         return jsonify({'result': False})
@@ -63,6 +67,7 @@ def delete_item(task_id):
 
 @api.route('/tasks/<int:task_id>', methods=['PUT'])
 def update_item(task_id):
+    storage = current_app.config["data_manager"]
     if not request.json or 'name' not in request.json or 'description' not in request.json \
             or 'date_start' not in request.json or 'duration' not in request.json:
         abort(400)
@@ -71,8 +76,8 @@ def update_item(task_id):
     _date_start = request.json['date_start']
     _duration = request.json['duration']
     if _name and _description and _date_start and _duration:
-        if DataManager.update_task_info(task_id, _name, _description, _date_start, _duration):
-            item = list(filter(lambda t: t.task_id == task_id, DataManager.get_all()))
+        if storage.update_task_info(task_id, _name, _description, _date_start, _duration):
+            item = list(filter(lambda t: t.task_id == task_id, storage.get_all()))
             data = simplejson.dumps({"task": item}, indent=4, for_json=True)
             return Response(data, status=200, mimetype='application/json')
         else:
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='task-scheduler',
+    version='1.0.0',
+    packages=['model', 'config', 'router', 'controller'],
+    url='https://github.com/100-gram/task-scheduler.git',
+    license='MIT',
+    author='100-gram',
+    author_email='',
+    description='Python realization of task scheduler'
+)
